e283645cb9e8918648827560c579bbe05c15e04d
cazanna <goldf3219@gmail.com>
2023-02-05 22:18:42 +0800

    refactor: layout scss code split

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,    
-    MainComponent
+    MainComponent, ChatComponent
   ],
   imports: [
     BrowserModule,
